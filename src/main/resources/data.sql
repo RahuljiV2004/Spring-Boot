3d8d7381277cd5f228c8645f7f7d99f99f3c8654
RahuljiV2004 <rahuljivisu2004@gmail.com>
2025-06-15 02:15:39 +0530

    for schema and populating data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into student(rollno,name,marks) values (101,'Rahul',100);
+insert into student(rollno,name,marks) values (102,'Ashok',100);
+insert into student(rollno,name,marks) values (103,'Laks',100);
+insert into student(rollno,name,marks) values (104,'Dinesh',100);
+insert into student(rollno,name,marks) values (105,'Sri',100);
+insert into student(rollno,name,marks) values (106,'Santhosh',100);
+insert into student(rollno,name,marks) values (107,'Praveen',100);
