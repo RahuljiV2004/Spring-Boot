29e34e71f1478d347f72ae604bf5a81dc88a3155
RahuljiV2004 <rahuljivisu2004@gmail.com>
2025-06-15 02:14:00 +0530

    Added logics for services

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,28 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.demo.model.Student;
+import com.example.demo.services.StudentServices;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Java kathukalam maame");
+
+		ApplicationContext context=SpringApplication.run(DemoApplication.class, args);
+		Student s=context.getBean(Student.class);
+		s.setRollno(123);
+		s.setName("rahul");
+		s.setMarks(100);
+		StudentServices service=context.getBean(StudentServices.class);
+		service.addStudent(s);
+		List<Student>students=service.getStudent();
 	}
 
 }
